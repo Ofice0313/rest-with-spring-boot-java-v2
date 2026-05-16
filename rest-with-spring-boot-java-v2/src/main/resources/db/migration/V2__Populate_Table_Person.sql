10bc75b47b548cd8b49143a27de1d88a3497f5c7
Marcelo Caleb Júlio Ofice <marcelocalebjulioo@gmail.com>
2026-05-16 18:48:42 +0200

    Working Migrations and Flyway

---
+++ rest-with-spring-boot-java-v2/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,7 @@
+INSERT INTO `tb_person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'Luis Cabral - Drenagem(Mabotine)', 'Marcelo', 'Male', 'Ofice'),
+	(2, 'Ndlavela', 'Leocadia', 'Female', 'Simbine'),
+	(3, 'Liberdade', 'Delmira', 'Female', 'Simbine'),
+	(4, 'Beira-Manga', 'Samiro', 'Male', 'Simbine'),
+	(5, 'Tete', 'Leong', 'Male', 'Fat Wa'),
+	(6, 'Beira', 'Assis', 'Male', 'Junior Naterra');
